bc1420fca3c1c59bfe022877ad648c59d31a385e
Cecilia Suderman <csmyth7182@gmail.com>
2014-05-18 19:23:11 -0600

    Initial commit
    Adding preexisting project work to start using version control

---
+++ app/controllers/display_controller.rb
@@ -0,0 +1,19 @@
+class DisplayController < ApplicationController
+  def home
+  end
+  
+  def mission
+  end
+  
+  def who
+  end
+  
+  def product
+  end
+  
+  def sponsor
+  end
+  
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cnctech::Application.config.secret_key_base = 'd31ac957301f4300000364facc1957ac9afe72398faa60c9b1ff9ab02d033c62cb6a1109bfc864c558d08344f398e54323a52dbc1e1ccd200f0e5c2873615da6'
